7ff8d8e338d8e58d09b5104a27ca8fa0ed6a2f3f
Halldor <halldorr@gmail.com>
2016-03-16 15:38:54 +0000

    Mapper

---
+++ src/hotelsearch/System/SqlMapper.java
@@ -0,0 +1,16 @@
+package HotelSearch.System;
+
+/**
+ * Created by Halldor on 16/03/16.
+ */
+public class SqlMapper {
+
+    private <TOut, TIn> TOut Map(TIn dataSet) {
+        return (TOut)dataSet;
+    }
+
+
+    private int foo () {
+        return 2;
+    }
+}
