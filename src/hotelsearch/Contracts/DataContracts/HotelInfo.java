3be8566eea4493b108bb65e9d5258375e181c92a
halldorrt <halldorr@gmail.com>
2016-03-17 16:08:09 +0000

    SQL string builder og fyrstu contractar

---
+++ src/hotelsearch/Contracts/DataContracts/HotelInfo.java
@@ -0,0 +1,19 @@
+package hotelsearch.Contracts.DataContracts;
+
+/**
+ * Created by halldorr on 16/03/16.
+ */
+public class HotelInfo {
+
+    public boolean Wifi;
+
+    public boolean Smoking;
+
+    public boolean Breakfast;
+
+    public double Rating;
+
+    //public Area area; uncomment when area class has been created
+
+    public String name;
+}
---
+++ src/hotelsearch/Contracts/SearchFilters/HotelSearchFilter.java
@@ -0,0 +1,14 @@
+package hotelsearch.Contracts.SearchFilters;
+
+import hotelsearch.Contracts.DataContracts.HotelInfo;
+
+import java.util.Date;
+
+/**
+ * Created by halldorr on 16/03/16.
+ */
+public class HotelSearchFilter extends HotelInfo{
+    public Date DateIn;
+
+    public Date DateOut;
+}
