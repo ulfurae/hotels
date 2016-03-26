a91287e66367fa7edffc3520d7065416bdf5c569
Halldor <halldorr@gmail.com>
2016-03-26 16:03:50 +0000

    Rename og færsla milli mappa. Bjó til package demo sem eru skrár sem enda ekki í forritinu sjálfu

--- src/HotelSearch/Presentation/Views/HotelMainView.java
+++ src/HotelSearch/Presentation/Views/HotelMainView.java
@@ -1,30 +1,24 @@
 package HotelSearch.Presentation.Views;
 
-import HotelSearch.HotelSearch;
-
 import javax.swing.*;
-import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import java.awt.*;
 
 /**
  * Created by Halldor on 22/03/16.
  */
-public class MainAppFrame extends JFrame {
+public class HotelMainView extends JFrame {
 
     public JFrame mainFrame = new JFrame("Search for hotels in Iceland");
 
     // Creates main frame for the hotel search app
-    public MainAppFrame() {
-
+    public HotelMainView() {
         mainFrame.setSize(600,700);
         mainFrame.setMinimumSize(new Dimension(500,400));
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
         mainFrame.setVisible(true);
     }
 
     public JFrame getFrame() {
-
         return mainFrame;
     }
 }
--- src/HotelSearch/program.java
+++ src/HotelSearch/program.java
@@ -4,13 +4,13 @@ import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 import HotelSearch.Presentation.Views.HotelSearchPanel;
-import HotelSearch.Presentation.Views.MainAppFrame;
+import HotelSearch.Presentation.Views.HotelMainView;
 import HotelSearch.Presentation.Presenters.SearchPanelPresenter;
 
 /**
  * Created by ulfurae on 22.3.2016.
  */
-public class startHotelSearchApp {
+public class program {
 
 
     //// START HOTEL SEARCH APP \\\\
@@ -18,12 +18,12 @@ public class startHotelSearchApp {
 
         setLook();
 
-        MainAppFrame frame = new MainAppFrame();
+        HotelMainView frame = new HotelMainView();
 
         HotelSearchPanel searchPanel = new HotelSearchPanel();
         SearchPanelPresenter presenter = new SearchPanelPresenter(searchPanel);
 
-        frame.getFrame().add(presenter.main);
+        frame.getFrame().add(searchPanel.getView());
     }
 
     static void setLook() {
