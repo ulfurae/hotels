21793d06302b9ce7022150b15deab91a7f719937
halldorrt <halldorr@gmail.com>
2016-03-29 21:14:16 +0000

    SQL string builder tilbúinn

--- src/HotelSearch/program.java
+++ src/HotelSearch/program.java
@@ -25,22 +25,6 @@ public class program {
     public static void main(String[] args) {
         setLook();
 
-        HotelSearchFilter hotel = new HotelSearchFilter();
-
-        hotel.wifi = true;
-        hotel.rating = 2.0;
-
-        Calendar cal = new GregorianCalendar();
-        cal.set(1990, 10, 13, 0, 0);
-        hotel.dateIn = new Date(cal.getTimeInMillis());
-        cal.set(2000, 10, 13, 0, 0);
-        hotel.dateOut = new Date(cal.getTimeInMillis());
-
-        SqlCustomQuery q = QueryStringBuilder.getSQLQueryString(hotel, "Hotel", new HotelQueryResolver());
-
-        System.out.println(q.sqlStatement);
-        System.out.println(q.values);
-
         HotelMainViewPresenter presenter = new HotelMainViewPresenter(new HotelMainView(),
                                                                       new FrontPageSearchPanel(),
                                                                       new FrontPageSearchPanel(),
