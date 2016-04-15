5a8f7b05b01e14b05e148d6fd7f749d26f3f5286
halldorrt <halldorr@gmail.com>
2016-04-15 16:50:37 +0000

    SearchPanel refactoring

--- src/HotelSearch/Presentation/Views/SearchPanel.java
+++ src/HotelSearch/Presentation/Views/SearchPanel.java
@@ -2,16 +2,11 @@ package HotelSearch.Presentation.Views;
 
 import HotelSearch.Presentation.Interfaces.ISearchPanel;
 import com.michaelbaranov.microba.calendar.DatePicker;
-
 import javax.swing.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyVetoException;
 import java.util.Date;
 
-/**
- * Created by Halldor on 22/03/16.
- */
 public class SearchPanel extends JPanel implements ISearchPanel{
     private JPanel searchPanel;
     private JPanel filterPanel;
@@ -25,45 +20,8 @@ public class SearchPanel extends JPanel implements ISearchPanel{
     private JComboBox areaComboBox;
     private JButton searchBtn;
 
-    public SearchPanel() {
-
-        dpDateIn.addActionListener(new dpInAction());
-        dpDateOut.addActionListener(new dpOutAction());
-    }
-
-    class dpInAction implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-
-            if(dpDateOut.getDate().before(dpDateIn.getDate())) {
-                try {
-                    //Date increment = DateUtils.addDays(today, 1);
-                    dpDateOut.setDate(dpDateIn.getDate());
-                } catch (PropertyVetoException e1) { e1.printStackTrace(); }
-            }
-        }
-    }
-
-    class dpOutAction implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-
-            if(dpDateOut.getDate().before(dpDateIn.getDate())) {
-
-                JOptionPane.showMessageDialog(null, "Can't select date prior to check-in date");
-                try {
-                    dpDateOut.setDate(dpDateIn.getDate());
-                } catch (PropertyVetoException e1) { e1.printStackTrace(); }
-            }
-        }
-    }
-
     public JPanel getView() {
         return searchPanel;
-    }
-
-    public String getHotelName() { return txtName.getText(); }
-
-    public int getAreaId() {
-         return (Integer) areaComboBox.getSelectedItem();
     }
 
     public String getAreaName() {
@@ -82,15 +40,23 @@ public class SearchPanel extends JPanel implements ISearchPanel{
         return chkBreakfast.isSelected();
     }
 
-    public Date getDateIn() {
-        return dpDateIn.getDate();
-    }
+    public Date getDateIn() { return dpDateIn.getDate(); }
 
     public Date getDateOut() {
         return dpDateOut.getDate();
     }
 
-    public void setSearchBtnAction(ActionListener evt) {
-        searchBtn.addActionListener(evt);
-    };
+    public void setDateOut(Date dateOut) {
+        try {
+            dpDateOut.setDate(dateOut);
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setDateInAction(ActionListener evt) { dpDateIn.addActionListener(evt); }
+
+    public void setDateOutAction(ActionListener evt) { dpDateOut.addActionListener(evt); }
+
+    public void setSearchBtnAction(ActionListener evt) { searchBtn.addActionListener(evt); }
 }
